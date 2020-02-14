df339178413a9459bb1c294224978c6f6f8fc8a5
Saske23 <46933742+Saske23@users.noreply.github.com>
2020-02-14 20:09:19 +0300

    это 2 но не до конца правильная

--- ol.py
+++ ol.py
@@ -1,37 +1,46 @@
 import os
 import sys
-
 import pygame
 import requests
+from pygame.locals import *
 
-response = None
-coords_1 = str(input())
-coords_2 = str(input())
-m = str(input())
-map_request = ('https://static-maps.yandex.ru/1.x/?l=sat&ll=' + coords_1 + '%2C-' + coords_2 + '&z=' + m)
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-
-# Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+
+def draw():
+    map_request = ('https://static-maps.yandex.ru/1.x/?l=sat&ll=' + coords_1 + '%2C-' + coords_2 + '&z=' + str(m))
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    screen.blit(pygame.image.load(map_file), (0, 0))
+
+
+response = None
+coords_1 = '140'
+coords_2 = '39'
+m = 3
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                m += 1
+            elif event.button == 3:
+                m -= 1
+    draw()
+    pygame.display.update()
+    pygame.display.flip()
 pygame.quit()
 
-# Удаляем за собой файл с изображением.
 os.remove(map_file)
